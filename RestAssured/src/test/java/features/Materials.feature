0b0b2a1d0f4a70ef4ac03949e5b29ad6b0385c2b
Juan Camilo Mejía Montoya <juan.mejiam@perficient.com>
2022-11-23 11:11:55 -0500

    Fixes

--- RestAssured/src/test/java/features/Materials.feature
+++ RestAssured/src/test/java/features/Materials.feature
@@ -1,8 +1,8 @@
 Feature: Get a list of existing Materials in the system
 
   Scenario Outline: Get a Materials list from API
-    Given User sends a GET request to the "<Resource>" (Materials)
+    Given User sends a GET request to the endpoint (Materials)
     Then  User validates that the "<Attribute>" has this "<Value>" (Materials)
     Examples:
-      | Resource      | Attribute       | Value      |
-      | /v2/divisions | Data.divisionId | -762496764 |
+      | Attribute       | Value      |
+      | Data.divisionId | -762496764 |
--- RestAssured/src/test/java/steps/Materials.java
+++ RestAssured/src/test/java/steps/Materials.java
@@ -5,25 +5,26 @@ import io.cucumber.java.en.Then;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseOptions;
 import utilities.APIUtilities;
+import utilities.FetchEnvParamsValues;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItem;
 
-public class Divisions {
+public class Materials {
 
     private ResponseOptions<Response> response;
 
-    @Given("User sends a GET request to the endpoint \\(Divisions)")
-    public void userSendsAGETRequestToTheEndpointDivisions() {
-        String resource = "/v2/divisions";
+    @Given("User sends a GET request to the endpoint \\(Materials)")
+    public void userSendsAGETRequestToTheEndpointMaterials() {
+        String resource = "/v2/divisions" + FetchEnvParamsValues.fetchParamValue("DIVISION_ID") + "/materials";
         response = APIUtilities.get(resource);
     }
 
-    @Then("User validates that the {string} has this {string} \\(Divisions)")
-    public void userValidatesThatTheHasThisDivisions(String attribute, String divisionId) {
+    /*@Then("User validates that the {string} has this {string} \\(Materials)")
+    public void userValidatesThatTheHasThisMaterials(String arg0, String arg1) {
         assertThat(response
                 .getBody()
                 .jsonPath()
                 .get(attribute), hasItem(Integer.valueOf(divisionId)));
-    }
+    }*/
 }
